43a0b8caf1dd8b6393b011192564de87b8a0fa98
Crudu Cristian <cristian.crudu@usm.md>
2025-04-13 18:15:07 +0300

    Add task

---
+++ README.md
@@ -0,0 +1,3 @@
+# Atestare 2
+
+Analizați și recreați proiectul: [https://react-at2.vercel.app/](https://react-at2.vercel.app/) 
---
+++ src/constants/stages.js
@@ -0,0 +1,10 @@
+export const stages = [
+    "https://i.imgur.com/kReMv94.png",
+    "https://i.imgur.com/UFP8RM4.png",
+    "https://i.imgur.com/9McnEXg.png",
+    "https://i.imgur.com/vNAW0pa.png",
+    "https://i.imgur.com/8UFWc9q.png",
+    "https://i.imgur.com/rHCgIvU.png",
+    "https://i.imgur.com/CtvIEMS.png",
+    "https://i.imgur.com/Z2mPdX0.png",
+];
